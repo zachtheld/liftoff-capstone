bf0a5f03b0c03884763e4fb5a9662dbdbf86b9c9
Zach Held <ztheld@gmail.com>
2018-08-01 15:47:30 -0500

    First commit

---
+++ src/main/java/org/launchcode/liftoffcapstone/LiftoffCapstoneApplication.java
@@ -0,0 +1,12 @@
+package org.launchcode.liftoffcapstone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiftoffCapstoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiftoffCapstoneApplication.class, args);
+	}
+}
