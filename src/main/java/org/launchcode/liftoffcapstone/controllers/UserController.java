81abd68a320af0a39fc828196894d7dc957950a1
Zach Held <ztheld@gmail.com>
2018-08-01 17:57:03 -0500

    Registered conformation

--- src/main/java/org/launchcode/liftoffcapstone/controllers/UserController.java
+++ src/main/java/org/launchcode/liftoffcapstone/controllers/UserController.java
@@ -4,15 +4,27 @@ package org.launchcode.liftoffcapstone.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
+
 
 @Controller
 @RequestMapping("user")
 public class UserController {
 
-    @RequestMapping(value = "")
-    public String index(Model model) {
+    static ArrayList<String> user = new ArrayList<>();
+
+    @RequestMapping(value = "register", method = RequestMethod.GET)
+    public String registerForm(Model model) {
         model.addAttribute("title", "Register");
 
         return "user/register";
+    }
+    @RequestMapping(value = "register", method = RequestMethod.POST)
+    public String processRegisterForm(@RequestParam String name, Model model) {
+        user.add(name);
+        return "user/registered";
     }
 }
